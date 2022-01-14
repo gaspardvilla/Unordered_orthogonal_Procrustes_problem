e8aa65f646d724b6a213e08aa48dcc89316e4adc
gaspardvilla <37636524+gaspardvilla@users.noreply.github.com>
2022-01-14 03:10:32 +0100

    Begin chapter 4

---
+++ scripts/Section_4_2_2.m
@@ -0,0 +1,12 @@
+%% Description
+
+% The idea of this script is to produce the results of performances 
+% we interpret in the section 4.2.2 of the report. 
+
+
+%% Code
+close all
+clearvars
+clc
+
+%% Options setting
